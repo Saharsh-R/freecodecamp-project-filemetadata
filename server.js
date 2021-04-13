e0e73013cbe03e2ca1fe2a4aa46e976d54bee7f2
Saharsh-R <48884865+Saharsh-R@users.noreply.github.com>
2021-04-13 10:34:43 +0530

    initial

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 
 var app = express();
 
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,8 +14,12 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'),  (req, res) => {
+  if (!req.file) return res.send('Error. Please choose a file.')
+  var result = {name: req.file.originalname, type: req.file.mimetype, size: req.file.size}
+  if (!!req.body.comment) result.comment = req.body.comment
+  res.send(result)
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
